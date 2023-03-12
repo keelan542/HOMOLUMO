47b4aeef4682b13f2732c34ecc504c3ab0741ad2
keelan542 <keelanb542@gmail.com>
2023-03-12 18:49:20 +0000

    Added orbital energies to respective dictionaries for occupied and virtual orbitals

--- orbs.py
+++ orbs.py
@@ -9,16 +9,29 @@ while True:
 		file_name = input("Please enter the name of the file to analyse: ")
 		with open(file_name + ".log") as f:
 			for line in f:
+
+				# Once we reach the orbital section, start adding the orbitals to dicts (one for O and one for V)
 				if "Orbital energies and kinetic energies (alpha):" in line:
+
+					# Skip line after finding above string, as the line after this is the beginning of the orbital energies
 					next(f)
+
+					# Begin inner loop over the lines containing the orbital energies
 					for inner_line in f:
-						print(inner_line)
+
+						# Conditional block to check if we are at the end of the orbital energies
+						# or at an occupied orbital, or at a virtual orbital
+						if "Total kinetic energy from orbitals" in inner_line:
+							break
+						elif inner_line.split()[1] == "O":
+							occup[int(inner_line.split()[0])] = float(inner_line.split()[2])
+						elif inner_line.split()[1] == "V":
+							virt[int(inner_line.split()[0])] = float(inner_line.split()[2])
+
 	except FileNotFoundError:
 		print("Sorry, the file was not found.")
 	else:
 		break
-
-# Once we reach the orbital section, start adding the orbitals to dicts (one for O and one for V)
 
 # Locate HOMO and LUMO energies from dictionaries
 
