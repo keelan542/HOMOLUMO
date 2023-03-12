e294a2eba3444d389cff828317179aee7493e6a5
keelan542 <keelanb542@gmail.com>
2023-03-12 17:38:28 +0000

    Added comments describing the initial main steps of the program

---
+++ orbs.py
@@ -0,0 +1,9 @@
+# Ask user for the name of the .log file to analyse
+
+# Open up the file and start reading it
+
+# Once we reach the orbital section, start adding the orbitals to dicts (one for O and one for V)
+
+# Locate HOMO and LUMO energies from dictionaries
+
+# Calculate HOMO-LUMO gap, convert to eV and print this information out
