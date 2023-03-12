8b4a55ff4550dedfb28488f3690976a3c4923fc0
keelan542 <keelanb542@gmail.com>
2023-03-12 17:55:29 +0000

    Added code to open a file specified by user, and read line by line

--- orbs.py
+++ orbs.py
@@ -1,6 +1,15 @@
-# Ask user for the name of the .log file to analyse
-
-# Open up the file and start reading it
+# Ask user for the name of the file
+# Open up the file and start reading it, if the file exists
+while True:
+	try:
+		file_name = input("Please enter the name of the file to analyse: ")
+		with open(file_name + ".log") as f:
+			for line in f:
+				continue
+	except:
+		print("Sorry, the file was not found.")
+	else:
+		break
 
 # Once we reach the orbital section, start adding the orbitals to dicts (one for O and one for V)
 
